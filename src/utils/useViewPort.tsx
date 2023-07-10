7055873a5a4ac12517a78829aba93e7b54bb81b7
Gabriel <caampos22@gmail.com>
2023-07-10 07:25:58 -0300

    fix :bug:: fix window is not defined

--- src/utils/useViewPort.tsx
+++ src/utils/useViewPort.tsx
@@ -3,10 +3,13 @@
 import React from 'react'
 
 const useViewport = () => {
+  const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
+
   const [width, setWidth] = React.useState(window.innerWidth);
   const breakpoint = 560;
 
   React.useEffect(() => {
+    if (!isBrowser()) return
     const handleWindowResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleWindowResize);
     return () => window.removeEventListener("resize", handleWindowResize);
